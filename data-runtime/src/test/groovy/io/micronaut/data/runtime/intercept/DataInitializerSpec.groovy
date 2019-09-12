2ef0ac817f6b7d20980aac48a2317f49018a1ebb
Borja Bravo <borjab@gmail.com>
2019-09-12 22:50:15 +0200

    [Test][Refactor] Made clear when the locale is being used + constants

--- data-runtime/src/test/groovy/io/micronaut/data/runtime/intercept/DataInitializerSpec.groovy
+++ data-runtime/src/test/groovy/io/micronaut/data/runtime/intercept/DataInitializerSpec.groovy
@@ -18,6 +18,19 @@ class DataInitializerSpec extends Specification {
 
     static def DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd")
 
+    // All this dates represent just 1 day after UNIX Epoch time
+    static def TEST_MILLISECONDS = 24 * 60 * 60 * 1000L
+    static def TEST_DATE = new Date(TEST_MILLISECONDS)
+    static def TEST_SQL_DATE = new java.sql.Date(TEST_MILLISECONDS)
+    static def TEST_INSTANT = Instant.ofEpochMilli(TEST_MILLISECONDS)
+    static def TEST_TIMESTAMP = new Timestamp(TEST_MILLISECONDS)
+    static def TEST_LOCAL_DATE = LocalDate.of(1970, 1, 2)
+    static def TEST_OFFSET_DATE_TIME = TEST_LOCAL_DATE.atTime(LocalTime.of(2, 0))
+            .atOffset(ZoneOffset.of("+02:00"))
+
+    // This date will be built according with the default timezone depending on the locale
+    static def TEST_DATE_AT_LOCAL_ZONE = DATE_FORMAT.parse("1970-01-02")
+
     @Unroll
     def "test date conversion #obj to #targetType"() {
         given:
@@ -29,30 +42,18 @@ class DataInitializerSpec extends Specification {
         then:
             result == expectedValue.get()
         where:
-            obj                                          || targetType     || result
-            DATE_FORMAT.parse("1970-01-02")              || LocalDate      || LocalDate.parse("1970-01-02")
-            DATE_FORMAT.parse("1970-01-02")              || LocalDateTime  || LocalDate.parse("1970-01-02").atStartOfDay()
-            DATE_FORMAT.parse("1970-01-02")              || OffsetDateTime || LocalDate.parse("1970-01-02").atStartOfDay().atZone(ZoneId.systemDefault()).toOffsetDateTime()
-            LocalDate.parse("1970-01-02")                || Date           || DATE_FORMAT.parse("1970-01-02")
-            LocalDate.parse("1970-01-02").atStartOfDay() || Date           || DATE_FORMAT.parse("1970-01-02")
-            new Date(1500000000000)                      || Instant        || Instant.ofEpochMilli(1500000000000)
-            Instant.ofEpochMilli(1500000000000)      || Date           || new Date(1500000000000)
-            LocalDate.parse("1970-01-02")
-                    .atTime( LocalTime.of(2,0))
-                    .atOffset(ZoneOffset.of("+02:00")) || java.sql.Date  || new java.sql.Date(24 *60 * 60 * 1000)
-            LocalDate.parse("1970-01-02")
-                .atTime( LocalTime.of(2,0))
-                .atOffset(ZoneOffset.of("+02:00"))     || Date          || new Date(24 *60 * 60 * 1000)
-            LocalDate.parse("1970-01-02")
-                .atTime( LocalTime.of(2,0))
-                .atOffset(ZoneOffset.of("+02:00"))     || Long          || 24 *60 * 60 * 1000
-            LocalDate.parse("1970-01-02")
-                .atTime( LocalTime.of(2,0))
-                .atOffset(ZoneOffset.of("+02:00"))     || Timestamp     || new Timestamp(24 *60 * 60 * 1000)
-            LocalDate.parse("1970-01-02")
-                .atTime( LocalTime.of(2,0))
-                .atOffset(ZoneOffset.of("+02:00"))     || LocalDateTime || LocalDate.parse("1970-01-02").atTime( LocalTime.of(2,0))
-
-
+            obj                            || targetType     || result
+            TEST_DATE_AT_LOCAL_ZONE        || LocalDate      || TEST_LOCAL_DATE
+            TEST_DATE_AT_LOCAL_ZONE        || LocalDateTime  || TEST_LOCAL_DATE.atStartOfDay()
+            TEST_DATE_AT_LOCAL_ZONE        || OffsetDateTime || TEST_LOCAL_DATE.atStartOfDay().atZone(ZoneId.systemDefault()).toOffsetDateTime()
+            TEST_LOCAL_DATE                || Date           || TEST_DATE_AT_LOCAL_ZONE
+            TEST_LOCAL_DATE.atStartOfDay() || Date           || TEST_DATE_AT_LOCAL_ZONE
+            TEST_DATE                      || Instant        || TEST_INSTANT
+            TEST_INSTANT                   || Date           || TEST_DATE
+            TEST_OFFSET_DATE_TIME          || java.sql.Date  || TEST_SQL_DATE
+            TEST_OFFSET_DATE_TIME          || Date           || TEST_DATE
+            TEST_OFFSET_DATE_TIME          || Long           || TEST_MILLISECONDS
+            TEST_OFFSET_DATE_TIME          || Timestamp      || TEST_TIMESTAMP
+            TEST_OFFSET_DATE_TIME          || LocalDateTime  || TEST_LOCAL_DATE.atTime( LocalTime.of(2,0))
     }
 }
